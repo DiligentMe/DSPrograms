66591de2b076b5c10d0b39e283e3301b64fca424
DiligentMe <shish.ashish@gmail.com>
2020-08-28 08:06:57 +0530

    List as array - Create, Traverse, Insert, Delete, Search and Update

---
+++ List/ListAsArray.py
@@ -0,0 +1,14 @@
+import os
+
+at = int(input())
+lookingFor = int(input())
+updateKth = int(input())
+arr = [ ]          #Empty List
+
+arr.insert(0, 10)   #Insertion in the List at first index
+print(arr)
+
+arr.append(120)       #Insertion in the List at last index
+print(arr)
+
+arr.insert(at, 20)
